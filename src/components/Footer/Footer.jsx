0f17ead21114e575bab391eaece2f2b05867e6b2
Suraj kumar chaurasiya <surajchaurasiya8009448@gmail.com>
2023-06-30 17:51:57 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
       {/* <img src={Wave} alt="" style={{ width: "100%" }} /> */}
       <div className="f-content">
         <div className="f-icons">
-         <a href="https://instagram.com/suraj_r00?igshid=MzNlNGNkZWQ4Mg=="> <Insta color="white" size={"2rem"} /></a>
-          <a href="https://www.facebook.com/profile.php?id=100009176339719"><Facebook color="white" size={"2rem"} /></a>
-          <a href="https://github.com/surajrac800"><Github color="white" size={"2rem"} /></a> 
+         <a href="https://instagram.com/suraj_r00?igshid=MzNlNGNkZWQ4Mg=="> <Insta color="#3586ff" size={"2rem"} /></a>
+          <a href="https://www.facebook.com/profile.php?id=100009176339719"><Facebook color="#3586ff" size={"2rem"} /></a>
+          <a href="https://github.com/surajrac800"><Github color="#3586ff" size={"2rem"} /></a> 
         </div> 
         <span>8009448518</span> 
          <span>Er. Suraj copyright &copy; 2022</span>
