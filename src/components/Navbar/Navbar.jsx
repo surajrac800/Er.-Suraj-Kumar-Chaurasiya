2ef404ecbffe89859ac67aadc2b4dc7c180caeed
Suraj kumar chaurasiya <surajchaurasiya8009448@gmail.com>
2023-07-02 15:12:39 +0530

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-      <div className="n-name">Er. Suraj</div>
+      <div className="n-name">Suraj</div>
         <Toggle />
       </div>
       {/* right */}
